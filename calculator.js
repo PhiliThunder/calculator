1a2aaffb571ad9ca51d4074e80f2a7dea36b2b52
Philip Dahlberg <philiparonkd@gmail.com>
2024-05-03 15:42:59 +0200

    Basic functions and operation vars added

---
+++ calculator.js
@@ -0,0 +1,17 @@
+//Operation variables
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+//Basic math functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {
+    return num1 / num2;
+}
